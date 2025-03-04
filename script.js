21563707496a262aacafe6204f2411bd7d7baf2a
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-03-04 12:20:09 +0530

    fix: Add todo by pressing enter

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 "use strict";
 
 let addTask = () => {
-  let btnClick = document.getElementById("addTask");
+  let inputValue = document.getElementById("addTaskValue");
+  let task = inputValue.value.trim();
 
-  btnClick.addEventListener("click", (event) => {
-    let inputValue = document.getElementById("addTaskValue");
-    let task = inputValue.value;
-
-    if (!task) {
-      alert("Task cannot be empty");
-    } else {
-      // console.log(task);
-      let listItem = document.createElement("li");
-      listItem.innerHTML = `<span class="task-text">${task}</span>
+  if (!task) {
+    alert("Task cannot be empty");
+    return;
+  }
+  let listItem = document.createElement("li");
+  listItem.innerHTML = `<span class="task-text">${task}</span>
       <div class="task-actions">
         <button class="editTask">Edit</button>
         <button class="deleteTask">Delete</button>
       </div>`;
-      listItem.className = "task";
-      document.querySelector("ul").appendChild(listItem);
-      inputValue.value = "";
-    }
-  });
+  listItem.className = "task";
+  document.querySelector("ul").appendChild(listItem);
+  inputValue.value = "";
 };
 
-addTask();
+document.getElementById("addTask").addEventListener("click", addTask);
+document
+  .getElementById("addTaskValue")
+  .addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
 
 let removeTask = () => {
   let todoList = document.querySelector("#todoList");
